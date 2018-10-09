b4fdb76cf9f25b2f43caeb3a04c2df31e3a0a875
ghoshben <sg2052@cse.jgec.ac.in>
2018-10-09 23:45:38 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+{
+  "name": "Cloud Encoe Sample",
+  "description": Cloud Encoe Sample",
+  "repository": "https://github.com/ghoshben/snickers",
+  "logo": "https://node-js-sample.herokuapp.com/node.png",
+  "keywords": ["node", "express", "static"]
+}
